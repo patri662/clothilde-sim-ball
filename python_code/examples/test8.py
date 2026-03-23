c029a7ad31c60cee3f7912716b156ad46757ce17
fcoltraro <fcoltraro@iri.upc.edu>
2026-03-23 11:59:15 +0100

    Update test8.py

--- python_code/examples/test8.py
+++ python_code/examples/test8.py
@@ -9,7 +9,7 @@ np.set_printoptions(threshold=sys.maxsize)
 import time
 
 # Caida libre
-na = 22; nb = 19
+na = 20; nb = 20
 np.random.seed(10)
 X, T = createRectangularMesh(a = 1, b = 1, na = na, nb = nb, h = 0.75)
 
@@ -26,19 +26,19 @@ X += 0.0002*np.random.randn(X.shape[0],3)
 
 self = Cloth(X, T, seam); 
 dt = self.estimateTimeStep(L=1.5)
-self.setSimulatorParameters(dt = dt, thck = 0.99, mu_s = 0.35, str = 0.025*1e-4, shr = 20*1e-4, 
-                            tol = 0.0075, kappa = 1*1e-4,kappa_bnd = 0.2*1e-4, mu_f = 0.2)
+self.setSimulatorParameters(dt = dt, thck = 0.99, mu_s = 0.35, str = 0.005*1e-4, shr = 20*1e-4, 
+                            tol = 0.0075, kappa = 1*1e-4,kappa_bnd = 1*1e-4, mu_f = 0.2)
 self.plotMesh()
 
 tf = int(2.5/dt); t = np.linspace(0,2*np.pi,tf); freq = 3
-inds_ctr = [300]
+inds_ctr = [170]
 u = X[inds_ctr]
 
 start_time = time.time()
 for i in range(tf):
     self.simulate(u = u, control = inds_ctr)
 u = self.positions[inds_ctr]; 
-for i in range(tf):
+for i in range(0*tf):
     u[:,1] += 0.005*np.sin(freq*t[i])
     self.simulate(u = u, control = inds_ctr)
 inds_ctr = []
@@ -49,4 +49,6 @@ for i in range(tf):
 print('Time:',time.time()-start_time)
 print('Average iterations',self.total_iters/(len(self.history_pos)-1))
 
-self.makeMovie(speed=5,repeat=False,smooth=2)
+self.makeMovie(speed=4,repeat=True,smooth=2)
+
+#kernprof -l -v test8.py > perfil_selfcols8.txt
